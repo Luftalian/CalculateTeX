8cf89132abb1afc486846707a6cec84ffc3e6a09
Luftalian <springtechtree@gmail.com>
2023-10-14 02:36:44 +0900

    fix: LICENSE

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,3 @@
-/*---------------------------------------------------------
- * Copyright (C) Microsoft Corporation. All rights reserved.
- *--------------------------------------------------------*/
-
 import * as vscode from 'vscode';
 import axios from 'axios';
 
